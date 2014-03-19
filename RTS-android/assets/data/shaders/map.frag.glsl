29862dc57bf6be91712081b845d0d1f6704fe5cc
tsagrista <tsagrista@ari.uni-heidelberg.de>
2014-03-19 15:27:36 +0100

    -Gunner image expanded to make room for shadow -Fixed glsl shadows -All map elements are now objects

--- RTS-android/assets/data/shaders/map.frag.glsl
+++ RTS-android/assets/data/shaders/map.frag.glsl
@@ -14,32 +14,28 @@ varying vec4 v_position;
 varying vec2 v_texCoords;
 
 uniform sampler2D u_texture;
-uniform vec2 screenRes;
+uniform vec2 u_viewport_size;
+uniform vec2 u_camera_offset;
 
 // Vectors with x, y, radius for all the entities
-uniform int lightCount;
-uniform int shadowCount;
-uniform vec3 lights[MAX_LIGHTS];
-uniform vec4 shadows[MAX_LIGHTS];
-uniform vec2 cameraPosition;
-
+uniform int u_light_count;
+uniform vec3 u_lights[MAX_LIGHTS];
 
 void main() {
 	float alpha = 0.0;
-	vec2 offset = cameraPosition - screenRes / 2.0;
 	
-	for(int i = 0; i < lightCount; i++){
-		vec3 light = lights[i];
+	for(int i = 0; i < u_light_count; i++){
+		vec3 light = u_lights[i];
 		// 1 - x
 		// 2 - y
 		// 3 - radius
 		float radius = light.z;
 		
 		// Determine camera position offset vector. Starts at (0,0)
-		vec2 lpos = light.xy - offset;
+		vec2 lpos = light.xy - u_camera_offset;
 		float dist = distance(gl_FragCoord.xy, lpos);
 		
-		if(dist >= 10 && dist <= radius){
+		if(dist <= radius){
 			// Area between shadow and visibility fade
 			alpha = 1.0;
 		}else if(dist > radius && dist < radius + radius * SMOOTH){
@@ -51,26 +47,11 @@ void main() {
 		}
 	}
 	
-	for(int i = 0; i < shadowCount; i++){
-		vec4 shadow = shadows[i];
-		// 1 - x
-		// 2 - y
-		// 3 - a**2
-		// 4 - b**2
-		vec2 spos = shadow.xy - offset - vec2(-1, 2);
-		float distSum = (pow(gl_FragCoord.x - spos.x, 2.0) / shadow.z)+(pow(gl_FragCoord.y - spos.y, 2.0) / shadow.a);
-		
-		if(distSum <= 1 && alpha > distSum) {
-			// Shadow zone
-			alpha = distSum;
-		}
-	}
-
 	// Default dim light
 	alpha += .15;
 	
 	vec4 texColor = texture2D(u_texture, v_texCoords);
-    gl_FragColor = vec4(texColor.rgb * alpha, texColor.a);
+    gl_FragColor = vec4(texColor.r * alpha, texColor.g * alpha, texColor.b * 1.2 * alpha, texColor.a);
 }
 
 
